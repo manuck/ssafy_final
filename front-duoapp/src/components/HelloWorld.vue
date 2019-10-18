0faff82ab8500907cb0d26cb3fcb99dcbaeaaeb0
권오현 <mmdkansk@gmail.com>
2019-10-18 17:48:13 +0900

    [OH] | 2019-10-18 | temp

--- front-duoapp/src/components/HelloWorld.vue
+++ front-duoapp/src/components/HelloWorld.vue
@@ -20,6 +20,7 @@
       <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
       <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
       <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
+      <!-- Oh hyun temp -->
     </ul>
     <h3>Ecosystem</h3>
     <ul>
