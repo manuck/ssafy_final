d1f7e3f00a018ff7c5f4f39ae41f3aba18152cf3
dmlimgo <dongmyeong.lim@gmail.com>
2019-10-23 17:55:45 +0900

    [DM] 메인페이지 레이아웃 구성

---
+++ front-duoapp/src/components/TopNavLogo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TopNavLogo = () => {
+    return (
+        <div className="topnav__logo">
+            <span>[logo]</span>
+        </div>
+    );
+};
+
+export default TopNavLogo;
