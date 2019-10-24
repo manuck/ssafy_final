71c82b1ce5d786c50e830981bf89285bbafeceb3
dmlimgo <dongmyeong.lim@gmail.com>
2019-10-24 18:00:52 +0900

    [DM] user 더미데이터 추가

--- front-duoapp/src/components/TopNav.js
+++ front-duoapp/src/components/TopNav.js
@@ -5,17 +5,67 @@ import { Link } from 'react-router-dom';
 import './TopNav.scss';
 
 const TopNav = () => {
+    const user = {
+        username: 'kbs3539@gmail.com',
+        nicknames: ['싹싹김치', '싹싹김치2', '갓김치'],
+        representationNickname: '싹싹김치',
+        tiers: {
+            tier: 'diamond',
+            rank: 'IV',
+            leaguePoint: 3000
+        },
+        recentGames: [
+            {
+                win: true,
+                kills: 10,
+                deaths: 1,
+                assists: 5,
+                champNo: 22
+            },
+            {
+                win: false,
+                kills: 10,
+                deaths: 2,
+                assists: 3,
+                champNo: 10
+            },
+            {
+                win: true,
+                kills: 2,
+                deaths: 10,
+                assists: 15,
+                champNo: 1
+            },
+            {
+                win: true,
+                kills: 6,
+                deaths: 4,
+                assists: 5,
+                champNo: 40
+            },
+            {
+                win: true,
+                kills: 9,
+                deaths: 8,
+                assists: 7,
+                champNo: 32
+            }
+        ],
+        majorPosition: 'bot',
+        minorPosition: 'top',
+        apiUpdatedAt: '2016-05-18T16:00:00Z',
+    }
     return (
         <div className="topnav">
             <div className="topnav__logo">
                 <span>DUO.GG</span>
             </div>
             <div className="topnav__user">
-                {/* 로그인 안 했을 때 */}
-                {/* 로그인 페이지로 이동 */}
-                {/* <Link to="/login">시작하기</Link> */}
-                {/* 로그인 했을 때 */}
-                <Link to="/profile">[nickname] &gt;</Link>
+                {user ? (
+                    <Link to="/profile">{user.representationNickname} &gt;</Link>
+                ) : (
+                    <Link to="/login">시작하기</Link>
+                )}
             </div>
         </div>
     );
