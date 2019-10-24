15e9593fcae181d0eb59ab4c5ebd519e98b82e3f
manuck <snc9000@naver.com>
2019-10-24 15:17:45 +0900

    [SM] 최신전적 5게임 list형식으로 console.log 찍음

--- back-duoapp/lolAPI/index.js
+++ back-duoapp/lolAPI/index.js
@@ -1,17 +1,15 @@
 const axios = require('axios');
-mykey = 'RGAPI-bbcddc08-746e-4253-bc78-ea99352ee6c5'
-let zzzzz = []
+mykey = 'RGAPI-88c34019-ab7a-4462-b8e2-5a5742a1dbff'
+
 exports.getLOLData = function () {
-    console.log('오호미') 
-    console.log('try')
+    // let recentlyGame = [[],[],[],[],[]]
     let summonerUrl, matchUrl, leagueUrl; 
     summonerUrl = `https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/마눅?api_key=${mykey}`
     axios.get(encodeURI(summonerUrl))
     .then( summonerData => { 
-        console.log('asdasdasdad')
         console.log(summonerData.data)
         let data = summonerData.data
-        console.log(data['id'])
+        // console.log(data['id'])
         let userId = data['id']
         let accountId = data['accountId']
         let gamelist = []
@@ -19,16 +17,17 @@ exports.getLOLData = function () {
         matchUrl = `https://kr.api.riotgames.com/lol/match/v4/matchlists/by-account/${accountId}?api_key=${mykey}`
         axios.get(encodeURI(leagueUrl))
         .then( userData => {
-            console.log(userData.data)
+            if (userData.data[0]) {
             let tier = userData.data[0]['tier']
             let rank = userData.data[0]['rank']
             let leaguePoint = userData.data[0]['leaguePoints']
             console.log(tier, rank, leaguePoint)
+            }
         }).catch (error => {
             console.error(error)
         })
         axios.get(matchUrl)
-        .then( matchData => {
+        .then( async (matchData) => {
             // console.log(matchData.data['matches'])
             let matches = matchData.data['matches']
             let cnt = 0
@@ -41,47 +40,55 @@ exports.getLOLData = function () {
                     break
                 }
             }
-            gamelist.map( async (game) => {
-                console.log(game)
+            let recentlyGame = gamelist.map( async (game) => {
+                // console.log(game)
                 let gameInfoUrl = `https://kr.api.riotgames.com/lol/match/v4/matches/${game}?api_key=${mykey}`
                 const gameData = await axios.get(gameInfoUrl)
                 let participantId = 0
+                // let index = 0     
                 for (let j = 0; j < gameData.data['participantIdentities'].length; j++){
                     if (gameData.data['participantIdentities'][j]['player']['accountId'] === accountId) {
                         participantId = gameData.data['participantIdentities'][j]['participantId']
-                        console.log(participantId)
                         break
                     }
                 }
+                // for (let j = 0; j < 5; j++) {
+                //     if (gamelist[j] === game){
+                //         index = j
+                //         break
+                //     }
+                // }
                 if (gameData.data['participants'][participantId-1]['stats']['win'] === true) {
-                    console.log('승리')
-                    console.log(gameData.data['participants'][participantId-1]['stats']['kills'], gameData.data['participants'][participantId-1]['stats']['deaths'], gameData.data['participants'][participantId-1]['stats']['assists'])
+                    // console.log('승리')
+                    // console.log(gameData.data['participants'][participantId-1]['championId'])
+                    // console.log(gameData.data['participants'][participantId-1]['stats']['kills'], gameData.data['participants'][participantId-1]['stats']['deaths'], gameData.data['participants'][participantId-1]['stats']['assists'])
+                    let champion = gameData.data['participants'][participantId-1]['championId']
                     let kills = gameData.data['participants'][participantId-1]['stats']['kills']
                     let deaths = gameData.data['participants'][participantId-1]['stats']['deaths']
                     let assists = gameData.data['participants'][participantId-1]['stats']['assists']
-                    zzzzz.push(['승리', kills, deaths, assists])
-                    return ['승리', kills, deaths, assists] 
+                    // recentlyGame[index] = ['승리', kills, deaths, assists, champion]
+                    return ['승리', kills, deaths, assists, champion] 
+                    // return '승리'
                 }
                 else {
-                    console.log('패배')
-                    console.log(gameData.data['participants'][participantId-1]['stats']['kills'], gameData.data['participants'][participantId-1]['stats']['deaths'], gameData.data['participants'][participantId-1]['stats']['assists'])
+                    // console.log('패배')
+                    // console.log(gameData.data['participants'][participantId-1]['championId'])
+                    // console.log(gameData.data['participants'][participantId-1]['stats']['kills'], gameData.data['participants'][participantId-1]['stats']['deaths'], gameData.data['participants'][participantId-1]['stats']['assists'])
+                    let champion = gameData.data['participants'][participantId-1]['championId']
                     let kills = gameData.data['participants'][participantId-1]['stats']['kills']
                     let deaths = gameData.data['participants'][participantId-1]['stats']['deaths']
                     let assists = gameData.data['participants'][participantId-1]['stats']['assists']
-                    zzzzz.push(['패배', kills, deaths, assists])
-                    return ['패배', kills, deaths, assists]
+                    // recentlyGame[index] = ['패배', kills, deaths, assists, champion]
+                    return ['패배', kills, deaths, assists, champion]
+                    // return '패배'
                 }
-                // console.log(gameData.data['participants'][participantId-1]['stats']['win'])
-                // console.log(gameData.data['participantIdentities'][participantId-1])
-                // participants
-                // participantIdentities
-                })
-                console.log(gamelist)
-            }).catch (error => {
-                console.error(error)
             })
-
-    
+            recentlyGame = await Promise.all(recentlyGame);
+            console.log(recentlyGame)
+        }).catch (error => {
+            console.error(error)
+        })
+           
     }).catch (error => {
         console.error(error)
     })
--- back-duoapp/routes/index.js
+++ back-duoapp/routes/index.js
@@ -9,11 +9,7 @@ router.get('/', function(req, res, next) {
 
 router.get('/test', (req, res) => {
     // lolAPI.makeGetRequest();
-    console.log('aaa')
     let info = lolAPI.getLOLData();
-    console.log('aaa')
-    console.log('여기여기')
-    console.log(info)
     res.json({'이이이잉':'앗살라마라이쿰'})
 })
 
