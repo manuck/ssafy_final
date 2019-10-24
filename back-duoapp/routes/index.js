55c870886f958a85ad97eb93e6a80c81d2020947
opalcat <noblesse3539@daum.net>
2019-10-24 18:03:05 +0900

    회원가입 30%

--- back-duoapp/routes/index.js
+++ back-duoapp/routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
+const apiRouter = require('./api');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  	res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
 });
+router.use('/api', apiRouter);
 
 module.exports = router;
