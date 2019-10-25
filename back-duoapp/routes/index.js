c76530eded677c3f8a533b5d006719a20586a044
manuck <snc9000@naver.com>
2019-10-25 17:21:16 +0900

    [SM] User 정보 업데이트

--- back-duoapp/routes/index.js
+++ back-duoapp/routes/index.js
@@ -26,12 +26,51 @@ router.get('/test', (req, res) => {
     // const nowUser = User.findById(`${myid}`)
     // console.log(nowUser)
     // const nowUser = User.findOne({_id:myid})
-    User.find({_id:myid},function(err,data){
-        if (err) return handleError(err);
-        console.log('find all data => ',data)
-    })
+
+    // const nsm = User.find({_id:myid},function(err,data){
+    //     if (err) return handleError(err);
+    //     console.log('find all data => ',data)
+    //     // data[0]['mytier']['tier'] = '플래티넘'
+    //     console.log(data[0]['tiers'])
+    //     // User.nickname = '무적꼬부기짱짱짱'
+    //     return data[0] 
+    // })
+    User.update({_id:myid}, {tiers:{tier:'벌레티넘', rank:'IV', leaguePoint:10}},
+    function(err, res) {
+        // if (err) { 
+        //     callback(err, null);
+        // } else { 
+        //     callback(null, res);
+        // }
+    });
+
+
+    // console.log('---------------------')
+    // console.log(nsm)
+    // console.log('---------------------')
+    // mydatadata[0]['tiers']['leaguePoint']]
     // console.log(nowUser)
     res.json({'이이이잉':'앗살라마라이쿰'})
 })
 
 module.exports = router;
+
+
+// app.put('url', (req, res) => {
+
+//     const modelId = req.body.model_id;
+//     const newName = req.body.name;
+
+//     MyModel.findById(modelId).then((model) => {
+//         return Object.assign(model, {name: newName});
+//     }).then((model) => {
+//         return model.save();
+//     }).then((updatedModel) => {
+//         res.json({
+//             msg: 'model updated',
+//             updatedModel
+//         });
+//     }).catch((err) => {
+//         res.send(err);
+//     });
+// });
