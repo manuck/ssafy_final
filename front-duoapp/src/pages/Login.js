e84be214b2782dc95ccc56d04894ea7a419ca123
dmlimgo <dongmyeong.lim@gmail.com>
2019-10-25 17:29:28 +0900

    [DM] 로그인페이지 구현(30%)

--- front-duoapp/src/pages/Login.js
+++ front-duoapp/src/pages/Login.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Login.scss';
 
 const Login = () => {
+    const [username, setUsername] = useState("");
+
+    const callLoginApi = e => {
+        fetch("https://localhost:4000/api/auth/login")
+            .then(res => res.json())
+            .then(json => {
+                setUsername(e.json.title);
+            })
+    };
+    
     return (
-        <div className="login">
-            login page
+        <div className="google__login">
+            <button onClick={callLoginApi} className="login__button">
+                구글로 로그인하기
+            </button>
         </div>
     );
 };
