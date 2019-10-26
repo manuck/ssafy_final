97f6a24deff6d9e789d93026d52ad0dd09cf9c80
dmlimgo <dongmyeong.lim@gmail.com>
2019-10-26 16:38:09 +0900

    [DM] Google 로그인 완료

--- front-duoapp/src/pages/Login.js
+++ front-duoapp/src/pages/Login.js
@@ -1,20 +1,30 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import './Login.scss';
 
 const Login = () => {
     const [username, setUsername] = useState("");
 
-    const callLoginApi = e => {
-        fetch("https://localhost:4000/api/auth/login")
-            .then(res => res.json())
-            .then(json => {
-                setUsername(e.json.title);
-            })
+    const callLoginApi = async(e) => {
+        try {
+            const res = await fetch('/api/auth/google')
+            console.log(res)
+            // const res = await axios.get('/api/auth/google')
+            // console.log('r', res.data)
+            // --------
+            // fetch("https://localhost:4000/api/auth/login")
+            //     .then(res => res.json())
+            //     .then(json => {
+            //         setUsername(e.json.title);
+            //     })
+        } catch (e) {
+            console.log('r', e);
+        }
     };
 
     return (
         <div className="google__login">
-            <a href="localhost:4000/api/auth/google" className="login__button">
+            <a href="http://localhost:4000/api/auth/google" className="login__button">
                 구글로 로그인하기
             </a>
             {/* <button onClick={callLoginApi} className="login__button">
