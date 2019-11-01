eb3570be18eb11e61457eeb405809629bea3b575
dmlimgo <dongmyeong.lim@gmail.com>
2019-11-01 16:51:54 +0900

    [DM] Recruit Detail 페이지 생성

--- front-duoapp/src/components/nav/TopNav.js
+++ front-duoapp/src/components/nav/TopNav.js
@@ -24,7 +24,7 @@ const TopNav = () => {
         const username = '';
         await res.json().then(data => {
             setUser(data);
-            console.log(data);
+            // console.log(data);
         });
     }
     // const user = {
