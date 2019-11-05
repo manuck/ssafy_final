22aa1702edac73ae20de6263be3693e287eb485b
manuck <snc9000@naver.com>
2019-11-05 17:39:30 +0900

    [SM] hotfix

--- front-duoapp/src/index.js
+++ front-duoapp/src/index.js
@@ -5,24 +5,10 @@ import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-
-const initialUser = '초기 값'
-
-const reducer = (state, action) => {
-    return state
-}
-
-const store = createStore(() => reducer(initialUser))
-
-console.log(store.getState())
 
 ReactDOM.render(
     <BrowserRouter>
-        <Provider store={store}>
             <App/>
-        </Provider>
     </BrowserRouter>,
    document.getElementById('root')
 );
